d569544c0d06902de26a824baace217df31079d3
Lantianor <lantianor@126.com>
2017-10-22 20:31:18 -0500

    Update test.go

--- test.go
+++ test.go
@@ -1,8 +1,18 @@
 package main
-var a = "test"
-var b string = "runoob.com"
-var c bool
+
+var x, y int
+var (  // 这种因式分解关键字的写法一般用于声明全局变量
+    a int
+    b bool
+)
+
+var c, d int = 1, 2
+var e, f = 123, "hello"
+
+//这种不带声明格式的只能在函数体中出现
+//g, h := 123, "hello"
 
 func main(){
-    println(a, b, c)
+    g, h := 123, "hello"
+    println(x, y, a, b, c, d, e, f, g, h)
 }
